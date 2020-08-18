c1a9ba56553a498eb3213abc5a821f2f33d91503
DJ Stewart <deshastewart1992@gmail.com>
2020-08-18 10:42:40 -0700

    Updates

---
+++ precache-manifest.1fa4a2cfc71ac9bef548dc632e1233e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7c433f46a7cb2a7f31cf7c75ee03cd",
+    "url": "/reactDrums/index.html"
+  },
+  {
+    "revision": "9c63f91343bcff0f8a95",
+    "url": "/reactDrums/static/css/main.ac6712f7.chunk.css"
+  },
+  {
+    "revision": "2833260fe23288334ad9",
+    "url": "/reactDrums/static/js/2.cb36e1e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactDrums/static/js/2.cb36e1e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c63f91343bcff0f8a95",
+    "url": "/reactDrums/static/js/main.62666088.chunk.js"
+  },
+  {
+    "revision": "17992519b26ce7f7f01a",
+    "url": "/reactDrums/static/js/runtime-main.323c254c.js"
+  },
+  {
+    "revision": "c277d414b8348b91314e2b18e9e708c0",
+    "url": "/reactDrums/static/media/bg.c277d414.jpg"
+  }
+]);
